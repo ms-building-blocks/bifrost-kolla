46083eebec1e223f0d43af3312130bb51aff8f51
Blaisonneau David <david.blaisonneau@orange.com>
2017-09-14 17:03:10 +0200

    add venv for bifrost
    Signed-off-by: Blaisonneau David <david.blaisonneau@orange.com>

--- run.sh
+++ run.sh
@@ -68,11 +68,14 @@ echo "
 # install ansible
 #-------------------------------------------------------------------------------
 "
-apt-get install -y software-properties-common python-setuptools \
+apt update
+apt install -y software-properties-common python-setuptools \
     python-dev libffi-dev libssl-dev git sshpass tree python-pip
 pip install --upgrade pip
 pip install cryptography
 pip install ansible
+pip install netaddr
+mkdir -p /etc/ansible
 echo "jumphost ansible_connection=local" > /etc/ansible/hosts
 
 echo "
@@ -82,55 +85,56 @@ echo "
 "
 ansible-playbook opnfv-bifrost-install.yaml
 ansible-playbook opnfv-bifrost-enroll-deploy.yaml
-#
-# echo "
-# #-------------------------------------------------------------------------------
-# # Prepare nodes
-# #-------------------------------------------------------------------------------
-# "
-# ansible-playbook -i /etc/bosa/ansible_inventory opnfv-wait-for-nodes.yaml
-# ansible-playbook -i /etc/bosa/ansible_inventory opnfv-prepare-nodes.yaml
-#
-# echo "
-# #-------------------------------------------------------------------------------
-# # Prepare OSA
-# #-------------------------------------------------------------------------------
-# "
-# ansible-playbook opnfv-osa-prepare.yaml
-# /opt/openstack-ansible/scripts/bootstrap-ansible.sh
-# ansible-playbook opnfv-osa-configure.yaml
-#
-# echo "
-# #-------------------------------------------------------------------------------
-# # Run OSA
-# #-------------------------------------------------------------------------------
-# "
-# cd /opt/openstack-ansible/playbooks
-# openstack-ansible setup-hosts.yml
-# openstack-ansible setup-infrastructure.yml
-# ansible galera_container -m shell -a "mysql -h localhost -e 'show status like \"%wsrep_cluster_%\";'"
-# openstack-ansible setup-openstack.yml
-#
-# echo "
-# #-------------------------------------------------------------------------------
-# # Fetch openrc and cert
-# #-------------------------------------------------------------------------------
-# "
-# CNT=$(ssh infra1 lxc-ls |grep utility)
-# ssh infra1 lxc-attach -n $CNT -- cat /root/openrc > /etc/bosa/openstack_openrc
-# scp infra1:/etc/ssl/certs/haproxy.cert  /etc/bosa/ca.cert
-# echo 'export OS_CACERT=/etc/bosa/ca.cert' >>  /etc/bosa/openstack_openrc
-#
-# echo "
-# #-------------------------------------------------------------------------------
-# # Prepare Infra
-# #-------------------------------------------------------------------------------
-# "
-# source /etc/bosa/openstack_openrc
-# ansible-playbook opnfv-openstack-prepare.yml
-#
-# echo "
-# #-------------------------------------------------------------------------------
-# # End
-# #-------------------------------------------------------------------------------
-# "
+
+echo "
+#-------------------------------------------------------------------------------
+# Prepare nodes
+#-------------------------------------------------------------------------------
+"
+ansible-playbook -i /etc/bosa/ansible_inventory opnfv-wait-for-nodes.yaml
+ansible-playbook -i /etc/bosa/ansible_inventory opnfv-prepare-nodes.yaml
+
+echo "
+#-------------------------------------------------------------------------------
+# Prepare OSA
+#-------------------------------------------------------------------------------
+"
+ansible-playbook opnfv-osa-prepare.yaml
+/opt/openstack-ansible/scripts/bootstrap-ansible.sh
+ansible-playbook opnfv-osa-configure.yaml
+
+echo "
+#-------------------------------------------------------------------------------
+# Run OSA
+#-------------------------------------------------------------------------------
+"
+cd /opt/openstack-ansible/playbooks
+openstack-ansible setup-hosts.yml
+openstack-ansible setup-infrastructure.yml
+ansible galera_container -m shell -a \
+    "mysql -h localhost -e 'show status like \"%wsrep_cluster_%\";'"
+openstack-ansible setup-openstack.yml
+
+echo "
+#-------------------------------------------------------------------------------
+# Fetch openrc and cert
+#-------------------------------------------------------------------------------
+"
+CNT=$(ssh infra1 lxc-ls |grep utility)
+ssh infra1 lxc-attach -n $CNT -- cat /root/openrc > /etc/bosa/openstack_openrc
+scp infra1:/etc/ssl/certs/haproxy.cert  /etc/bosa/ca.cert
+echo 'export OS_CACERT=/etc/bosa/ca.cert' >>  /etc/bosa/openstack_openrc
+
+echo "
+#-------------------------------------------------------------------------------
+# Prepare Infra
+#-------------------------------------------------------------------------------
+"
+source /etc/bosa/openstack_openrc
+ansible-playbook opnfv-openstack-prepare.yml
+
+echo "
+#-------------------------------------------------------------------------------
+# End
+#-------------------------------------------------------------------------------
+"
